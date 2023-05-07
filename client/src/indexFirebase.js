cb949703167ac224f633253417d79780a39a4fc0
VicenteGM123 <vicentegarcia@uc.cl>
2023-05-06 23:53:11 -0400

    fix: new api url (v3)

--- client/src/indexFirebase.js
+++ client/src/indexFirebase.js
@@ -37,7 +37,7 @@ messaging.onMessage((payload) => {
 });
 
 function sendSubscriptionIDToServer(subscription) {
-  fetch("https://pwabackendv2.onrender.com/subscribers", {
+  fetch("https://pwabackendv3.onrender.com/subscribers", {
     method: "post",
     headers: {
       Accept: "application/json",
