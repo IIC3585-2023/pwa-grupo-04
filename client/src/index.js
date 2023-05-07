a0fb4db7263e4f54f8958b23c596efc2711312b4
VicenteGM123 <vicentegarcia@uc.cl>
2023-05-06 23:43:30 -0400

    fix: move firebase sw to root folder

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ var db;
 window.addEventListener("load", async (e) => {
   if ("serviceWorker" in navigator) {
     try {
-      navigator.serviceWorker.register("serviceWorker.js");
+      navigator.serviceWorker.register("src/serviceWorker.js");
       console.log("SW registered");
     } catch (error) {
       console.log("SW failed");
